6c30666f0271078872432f9de2a22d6c8031c796
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-06-29 22:10:07 +0200

    Showcase adjustments

--- plugin.js
+++ plugin.js
@@ -4,35 +4,42 @@ if (Annotator.Plugin) {
 	Annotator.Plugin.WCTS = function (element) {
 		return {
 			pluginInit: function() {
-				function referenceTypeFieldLod(field, annotation) {
-					field.innerHTML = "";
-
-					let container = document.createElement("div");
-					field.appendChild(container);
-
-					let label = document.createElement("label");
-					label.innerHTML = "Reference Type:";
-					container.appendChild(label);
-
-					let select = document.createElement("select");
-					for (let i = 1; i <= 3; i++) {
-						let option = document.createElement("option");
-						option.value = "type" + i;
-						option.text = "Type " + i;
-						select.add(option);
-					}
-
+				function referenceTypeFieldLoad(field, annotation) {
 					if (annotation.referenceType) {
-						select.value = annotation.referenceType;
-					}
+						field.innerHTML = "";
 
-					container.appendChild(select);
+						let container = document.createElement("div");
+						field.appendChild(container);
+
+						let label = document.createElement("label");
+						label.innerHTML = "Reference Type: ";
+						container.appendChild(label);
+
+						let select = document.createElement("select");
+						let newsDetailPageOption = document.createElement("option");
+						newsDetailPageOption.value = "NewsDetailPage";
+						newsDetailPageOption.text = "News Detail Page";
+						select.add(newsDetailPageOption);
+
+						for (let i = 1; i <= 3; i++) {
+							let option = document.createElement("option");
+							option.value = "type" + i;
+							option.text = "Type " + i;
+							select.add(option);
+						}
+
+						if (annotation.referenceType) {
+							select.value = annotation.referenceType;
+						}
+
+						container.appendChild(select);
+					}
 				}
 
-				this.annotator.viewer.addField({ label: "Reference Type", load: referenceTypeFieldLod });
+				this.annotator.viewer.addField({ label: "Reference Type", load: referenceTypeFieldLoad });
 				this.annotator.editor.addField({
 					label: "Reference Type",
-					load: referenceTypeFieldLod,
+					load: referenceTypeFieldLoad,
 					submit: function(field, annotation) {
 						annotation.referenceType = field.getElementsByTagName("select")[0].value;
 					}
